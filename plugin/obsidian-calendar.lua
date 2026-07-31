11f8f60186b0107b28305511a182407077c800d8
Fabian Leeske <fabian.leeske@posteo.de>
2026-07-31 17:04:51 +0200

    feat: update usercmd with optional date

--- plugin/obsidian-calendar.lua
+++ plugin/obsidian-calendar.lua
@@ -1,4 +1,8 @@
 -- local cal = require("obsidiancalendar.calendar")
-vim.api.nvim_create_user_command("Calendar", function()
-  require("obsidiancalendar.calendar").open()
-end, {})
+vim.api.nvim_create_user_command("Calendar", function(opts)
+  if opts.fargs and opts.fargs[1] then
+    require("obsidiancalendar.calendar"):open(opts.fargs[1])
+  else
+    require("obsidiancalendar.calendar"):toggle()
+  end
+end, { nargs = "?" })
