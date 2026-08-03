63c0bf3b478cfb6063cefd50f5e59778ebec9574
Fabian Leeske <fabian.leeske@posteo.de>
2026-08-03 10:26:07 +0200

    fix(config): setup() not required

--- lua/obsidian-calendar/config.lua
+++ lua/obsidian-calendar/config.lua
@@ -1,12 +1,14 @@
 local M = {}
 
-local defaults = {
+M.defaults = {
   start_sunday = false, -- start week on sunday instead of monday
 }
 
 function M.setup(opts)
   opts = opts or {}
-  M.config = vim.tbl_deep_extend("force", defaults, opts)
+  M.config = vim.tbl_deep_extend("force", M.defaults, opts)
 end
+
+M.config = M.config or M.defaults
 
 return M
